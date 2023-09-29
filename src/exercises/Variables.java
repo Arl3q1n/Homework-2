c263ebeecdb1bcf1e1e1915c8db33f82c7cbf5ae
Arleq1n_ <arl3q1n@gmail.com>
2023-09-29 17:48:15 +0300

    Java Fundamentals

--- src/exercises/Variables.java
+++ src/exercises/Variables.java
@@ -5,19 +5,19 @@ public class Variables {
     public void Variables_() {
 
         long CurrentWorldPopulation = 7811080815L;
-        System.out.println(CurrentWorldPopulation);
+        System.out.println("Current World Population - " + CurrentWorldPopulation);
 
         int CarsProducedThisYear = 55235358;
-        System.out.println(CarsProducedThisYear);
+        System.out.println("Cars Produced This Year - " + CarsProducedThisYear);
 
         char FirstLetterInAlphabet = 'A';
-        System.out.println(FirstLetterInAlphabet);
+        System.out.println("First Letter In Alphabet - " + FirstLetterInAlphabet);
 
         boolean JavaIsAmazing = true;
-        System.out.println(JavaIsAmazing);
+        System.out.println("Java Is Amazing - " +JavaIsAmazing);
 
         double Pi = 3.14159;
-        System.out.println(Pi);
+        System.out.println("Pi - " + Pi);
 
     }
 }
